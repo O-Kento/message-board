d0d00e82614afbb750a5414f45d67b4bce978fa3
o_kento <kent_nm@hotmail.com>
2015-12-24 02:17:52 +0000

    create message model

--- db/migrate/20151222002053_create_messages.rb
+++ db/migrate/20151222002053_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def change
     create_table :messages do |t|
-      t.string :nsme
+      t.string :name
       t.string :body
 
       t.timestamps null: false
--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20151222002053) do
 
   create_table "messages", force: :cascade do |t|
-    t.string   "nsme"
+    t.string   "name"
     t.string   "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
