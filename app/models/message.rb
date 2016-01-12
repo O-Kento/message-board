92280df197243615ec5ae35c65c3c8f76f7571b6
o_kento <kent_nm@hotmail.com>
2016-01-12 19:51:59 +0000

    modified

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,6 @@
 class Message < ActiveRecord::Base
     validates :name , length: { maximum: 20} , presence: true
     validates :body , length: { minimum: 2, maximum: 30 } , presence: true
-    validates :age , length: { minimum: 1} , presence: true
+    validates :age , numericality: {greater_than_or_equal_to: 1 } , presence: true
 end
 
